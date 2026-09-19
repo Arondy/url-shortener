dc975d282e5c97d59f72ec8c5c1326ea76493e92
arondy <antongatcenko@gmail.com>
2026-09-19 18:31:49 +0300

    feat(app): добавлена инициализация компонентов

---
+++ internal/core/app.go
@@ -0,0 +1,44 @@
+package core
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/Arondy/url-shortener/internal/args"
+	"github.com/Arondy/url-shortener/internal/config"
+	"github.com/Arondy/url-shortener/internal/core/repository/in_memory"
+	"github.com/Arondy/url-shortener/internal/core/repository/postgres"
+	"github.com/Arondy/url-shortener/internal/core/service/url_shortener"
+	core_http "github.com/Arondy/url-shortener/internal/core/transport/http"
+	"github.com/Arondy/url-shortener/internal/core/transport/http/handlers/shorten"
+	"github.com/Arondy/url-shortener/internal/core/transport/http/middleware"
+	"go.uber.org/zap"
+)
+
+func Run(ctx context.Context, cfg *config.Config, args args.Args, logger *zap.SugaredLogger) error {
+	var shortenerRepo url_shortener.URLShortenerRepo
+
+	if args.InMemory {
+		shortenerRepo = in_memory.NewURLShortenerRepository()
+	} else {
+		db, err := postgres.NewDB(ctx, cfg.DB, logger)
+		if err != nil {
+			return err
+		}
+		defer db.Close()
+
+		shortenerRepo = postgres.NewURLShortenerRepository(db)
+	}
+
+	shortenerService := url_shortener.NewService(shortenerRepo)
+	shortenHander := shorten.NewShortenHandler(cfg.DomainURL, shortenerService)
+
+	var router http.Handler = core_http.NewRouter(shortenHander)
+	router = middleware.WrapInMiddleware(router, logger)
+	server := core_http.NewServer(router, cfg.HTTPServer, logger.Named("Server"))
+
+	if err := server.Run(ctx); err != nil {
+		return err
+	}
+	return nil
+}
