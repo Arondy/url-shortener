3038e6ebe10786fe01011a0ba9d97ee322c9890d
arondy <antongatcenko@gmail.com>
2026-09-20 00:52:05 +0300

    feat(errors): добавлена ошибка для истекшего TTL

--- internal/core/domain/errors.go
+++ internal/core/domain/errors.go
@@ -3,4 +3,5 @@ package domain
 import "errors"
 
 var ErrShortURLCodeNotFound = errors.New("provided short url code not found")
+var ErrShortURLCodeExpired = errors.New("provided short url code expired")
 var ErrShortURLCodeCollision = errors.New("short url code collision")
